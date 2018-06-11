e6bcba5f26606dfae4a547b99c294e79f6ba1159
Alicia Dinkel <alicia.dinkel@gmail.com>
2018-06-11 15:27:55 -0400

    working test specs

--- mergesort.js
+++ mergesort.js
@@ -1,19 +1,21 @@
 function split(wholeArray) {
-
     /* your code here to define the firstHalf and secondHalf arrays */
+    let arrlength = wholeArray.length, 
+        firstHalf = [], 
+        secondHalf = [];
   if (wholeArray.length === 1){
       return wholeArray;
   } else if (wholeArray.length % 2 === 0){
       // even length
-      let arrlength = wholeArray.length
-      let firstHalf = wholeArray.slice(0, (arrlength / 2));
-      let secondHalf = wholeArray.slice(arrlength / 2);
+      
+    firstHalf = wholeArray.slice(0, (arrlength / 2));
+    secondHalf = wholeArray.slice(arrlength / 2);
   } else {
       // odd length
-
+    // let oddArrLength = wholeArray.length + 1
+    firstHalf = wholeArray.slice(0, ((arrlength + 1) / 2));
+    secondHalf = wholeArray.slice((arrlength+1) / 2);
   }
-
-
     return [firstHalf, secondHalf];
 }
 
---
+++ mergesort.spec.js
@@ -0,0 +1,15 @@
+describe('Split Array function', function() {
+    it('is able to split an array into two halves', function() {
+        var sample = [4, 7, 3, 1];
+        var splitOutput = split(sample);
+        expect(splitOutput).toEqual([[4, 7], [3, 1]]);
+    });
+    it('returns original array if length is 1', function(){
+        var sample2 = [1];
+        expect(split(sample2)).toEqual([1]);
+    });
+    it('handles an odd array', function(){
+        var sample3 = [5, 8, 3, 9, 10];
+        expect(split(sample3)).toEqual([[5, 8, 3], [9, 10]]);
+    })
+  });
